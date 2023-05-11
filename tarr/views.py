a4050b07a11aa75e8fd13c7a9cafe5994d04573f
laura <lcastillo13@ucol.mx>
2023-05-11 10:35:21 -0600

    first commit

---
+++ tarr/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def singup(request):
+    if request.method == 'GET':
+        return render(request, 'singup.html', {
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            # register user
+            try:
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('info')
+            except:
+                return render(request, 'singup.html', {
+                    'form': UserCreationForm,
+                    'error': "El usuario ya existe"
+                })
+        return render(request, 'singup.html', {
+            'form': UserCreationForm,
+            'error': 'Las contraseñas no coinciden'
+        })
+
+@login_required
+def info(request):
+    return render(request, 'info.html')
+
+@login_required
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm,
+                'error': 'Usuario o contraseña incorrectas'
+            })
+        else:
+            login(request, user)
+            return redirect('info')
+
+@login_required      
+def cam(request):
+    return render(request, 'cam.html')
