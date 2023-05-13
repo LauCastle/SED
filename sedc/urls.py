b9f079b2f656848b5a69965096092e0d49089aa7
laura <lcastillo13@ucol.mx>
2023-05-12 19:08:03 -0600

    Actualización 1

--- sedc/urls.py
+++ sedc/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for sedc project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path
 from tarr import views
 
@@ -24,5 +8,6 @@ urlpatterns = [
     path('info/', views.info, name='info'),
     path('logout/', views.signout, name="logout"),
     path('signin/', views.signin, name='signin'),
-    path('cam/', views.cam, name='cam')
+    path('cam/', views.cam, name='cam'),
+    path('activate/<uidb64>/<token>', views.activate, name="activate")
 ]
