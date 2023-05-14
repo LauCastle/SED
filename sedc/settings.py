0da0d2ef5911b86fdb883b426169379b4c55ade1
laura <lcastillo13@ucol.mx>
2023-05-13 19:42:30 -0600

    Diseño de login y registro

--- sedc/settings.py
+++ sedc/settings.py
@@ -22,6 +22,8 @@ EMAIL_HOST_USER = EMAIL_HOST_USER
 EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
 EMAIL_PORT = EMAIL_PORT
 
+#APPEND_SLASH=False
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
